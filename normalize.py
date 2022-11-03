8a9462fda87912166bc9be5b874f73fb47b7ada9
georgeulll <103060386+georgeulll@users.noreply.github.com>
2022-11-03 20:35:09 -0100

    Add files via upload

---
+++ normalize.py
@@ -0,0 +1,54 @@
+import sys
+from pathlib import Path
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'ZIP': ARCHIVES
+}
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+def get_extension(filename: str) -> str:
+    return Path(filename).suffix[1:].upper()
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+        ext = get_extension(item.name)
+        fullname = folder / item.name
+        if not ext:
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+if __name__ == '__main__':
+    folder_for_scan = sys.argv[1]
+    print(f'Start in folder {folder_for_scan}')
+    scan(Path(folder_for_scan))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images svg: {SVG_IMAGES}')
+    print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Archives: {ARCHIVES}')
+    print(f'Types of files in folder: {EXTENSIONS}')
+    print(f'Unknown files of types: {UNKNOWN}')
+    print(FOLDERS[::-1])
