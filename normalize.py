6e56dea7a4d89b3ba013fe10c381e52018f5d1f9
georgeulll <103060386+georgeulll@users.noreply.github.com>
2022-11-04 18:17:42 -0100

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -4,16 +4,46 @@ JPEG_IMAGES = []
 JPG_IMAGES = []
 PNG_IMAGES = []
 SVG_IMAGES = []
+MOV_VIDEO = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MKV_VIDEO = []
+DOC_DOKUMENT = []
+DOCX_DOKUMENT = []
+TXT_DOKUMENT = []
+PDF_DOKUMENT = []
+XLSX_DOKUMENT = []
+PPTX_DOKUMENT = []
 MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
 MY_OTHER = []
-ARCHIVES = []
+GZ_ARCHIVE = []
+TAR_ARCHIVE = []
+ZIP_ARCHIVE = []
 REGISTER_EXTENSIONS = {
     'JPEG': JPEG_IMAGES,
     'PNG': PNG_IMAGES,
     'JPG': JPG_IMAGES,
     'SVG': SVG_IMAGES,
+    'MOV': MOV_VIDEO,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MKV': MKV_VIDEO,
     'MP3': MP3_AUDIO,
-    'ZIP': ARCHIVES
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'DOC': DOC_DOKUMENT,
+    'DOCX': DOCX_DOKUMENT,
+    'TXT': TXT_DOKUMENT,
+    'PDF': PDF_DOKUMENT,
+    'XLSX': XLSX_DOKUMENT,
+    'PPTX': PPTX_DOKUMENT,
+    'ZIP': ZIP_ARCHIVES,
+    'TAR': TAR_ARCHIVES,
+    'GZ': GZ_ARCHIVES
 }
 FOLDERS = []
 EXTENSIONS = set()
@@ -47,7 +77,21 @@ if __name__ == '__main__':
     print(f'Images jpeg: {JPEG_IMAGES}')
     print(f'Images jpg: {JPG_IMAGES}')
     print(f'Images svg: {SVG_IMAGES}')
+    print(f'Images png: {PNG_IMAGES}')
     print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Audio ogg: {OGG_AUDIO}')
+    print(f'Audio wav: {WAV_AUDIO}')
+    print(f'Audio amr: {AMR_AUDIO}')
+    print(f'Video mov: {MOV_VIDEO}')
+    print(f'Video avi: {AVI_VIDEO}')
+    print(f'Video mp4: {MP4_VIDEO}')
+    print(f'Video mkv: {MKV_VIDEO}')
+    print(f'Document doc: {DOC_DOKUMENT}')
+    print(f'Document docx: {DOCX_DOKUMENT}')
+    print(f'Document txt: {TXT_DOKUMENT}')
+    print(f'Document pdf: {PDF_DOKUMENT}')
+    print(f'Document xlsx: {XLSX_DOKUMENT}')
+    print(f'Document pptx: {PPTX_DOKUMENT}')
     print(f'Archives: {ARCHIVES}')
     print(f'Types of files in folder: {EXTENSIONS}')
     print(f'Unknown files of types: {UNKNOWN}')
